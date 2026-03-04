76411e86b181e97b1bb88021d97b09f56c6af7b2
Jonathan <jonisangui039@gmail.com>
2026-03-03 20:06:29 -0500

    fix conexion supabase ipv4

--- db.js
+++ db.js
@@ -3,13 +3,9 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     parseInt(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  family: 4, // ← forzar IPv4
+  family: 4,
 });
 
 async function inicializarDB() {
